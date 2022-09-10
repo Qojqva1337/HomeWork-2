420c0669f44dde209c95e6c2fcece399772bdd39
artur <artursuper10@gmail.com>
2022-09-10 15:58:08 +0300

    Задача 1-10, часть 2

---
+++ src/Main.java
@@ -0,0 +1,60 @@
+public class Main {
+    public static void main(String[] args) {
+       // Задача 1
+        System.out.println("Задача 1");
+        int cars = 4;
+        System.out.println("Значение переменной cars с типом int равно " + cars);
+        byte dogs = 5;
+        System.out.println("Значение переменной dogs с типом byte равно " + dogs);
+        short cats = 8;
+        System.out.println("Значение переменной cats с типом short равно " + cats);
+        long orangesCont = 50;
+        System.out.println("Значение переменной orangesCont с типом long равно " + orangesCont);
+        float pi = 3.1415F;
+        System.out.println("Значение переменной pi с типом float равно " + pi);
+        double e = 2.8;
+        System.out.println("Значение переменной e с типом double равно " + e);
+
+        // Задача 2
+        System.out.println("Задача 2");
+        float vr1 = 27.12F;
+        long vr2 = 987_678_965_549L;
+        double vr3 = 2.786;
+        boolean vr4 = false;
+        char vr5 = 569;
+        short vr6 = -159;
+        int vr7 = 27897;
+        byte vr8 = 67;
+
+        // Задача 3
+        System.out.println("Задача 3");
+        int teacher1 = 23;
+        int teacher2 = 27;
+        int teacher3 = 30;
+        int lists = 480;
+        int cont = lists / (teacher2 + teacher2 + teacher1);
+        System.out.println("На каждого ученика рассчитано " + cont + " листов бумаги");
+
+        // Задача 4
+        System.out.println("Задача 4");
+        int pro1 = 16;
+        int time1 = 20;
+        int time2 = 1440;
+        int time3 = 4320;
+        int time4 = 43200;
+        int time0 = 2;
+        int pro2 = (pro1 * time1) / time0;
+        int pro3 = (pro1 * time2) / time0;
+        int pro4 = (pro1 / time3) / time0;
+        int pro5 = (pro1 / time4) / time0;
+        System.out.println("За 20 минут машина произвела бутылок " + pro2 + " штук");
+        System.out.println("За 1440 минут машина произвела бутылок " + pro3 + " штук");
+        System.out.println("За 4320 минут машина произвела бутылок " + pro4 + " штук");
+        System.out.println("За 43200 минут машина произвела бутылок " + pro5 + " штук");
+
+        // Задача 5
+
+
+
+    }
+}
